642749cd53c49ea47e130debca29222a3b126e45
7devanle <7devanle@gmail.com>
2022-06-19 02:15:43 +0100

    crud update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# from crypt import methods
 from flask import Flask, jsonify, render_template,redirect, url_for,request
 from flask_sqlalchemy import SQLAlchemy
 import sys
@@ -40,10 +41,23 @@ def create_todo():
     if not error:
          return jsonify(body)
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
 
 @app.route('/')
 def index(): 
-    return render_template('index.html', data=Todo.query.all()) #return template of html to userview
+    return render_template('index.html', data=Todo.query.order_by('id').all()) #return template of html to userview
  
 
 if __name__ == '__main__':
